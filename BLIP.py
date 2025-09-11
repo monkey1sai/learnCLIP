8909eb9f6c6e31e9ad3d8b32fb89b2d904ba729f
monkey1sai <monkey1sai@hotmail.com>
2025-09-11 13:35:38 +0800

    update

--- BLIP.py
+++ BLIP.py
@@ -7,9 +7,11 @@ processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
 # 測試圖片
-url = "https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg"
-url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/cat.png"
-image = Image.open(requests.get(url, stream=True).raw)
+# url = "https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg"
+# image = Image.open(requests.get(url, stream=True).raw)
+
+url = "images/15375/symblepic/標準層陽台足下燈(EZ_SYMBLE).png"
+image = Image.open(url)
 
 # 處理圖片
 inputs = processor(images=image, return_tensors="pt")
